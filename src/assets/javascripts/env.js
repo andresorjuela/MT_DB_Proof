a85140a8dd214ec15d916ed7362777bb183e4a5e
gaumeister <derek@apigrate.com>
2020-02-13 15:42:59 -0600

    env.js should not be tracked

--- src/assets/javascripts/env.js
+++
@@ -1,7 +0,0 @@
-
-/** Generated environment file **/
-export default function(){
-return {
-NODE_ENV: "production", API_BASE_URL: "https://v73kddyeqk.execute-api.ap-southeast-1.amazonaws.com/Prod/api/v1", STATIC_ASSETS_PATH: "https://med10-assets.s3-ap-southeast-1.amazonaws.com", 
-}
-}
