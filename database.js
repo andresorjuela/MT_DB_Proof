6841c4be66d7b3d612af01e76ba4be44873cfe66
gaumeister <derek@apigrate.com>
2021-01-07 09:10:26 -0600

    Add formula, alphabetize reg order.

--- database.js
+++ database.js
@@ -24,31 +24,29 @@ module.exports = function (pool) {
   factory.Category               = ()=>{ return create_dao('t_category', 'category', standard_opts, null); };
   factory.Certificate            = ()=>{ return create_dao('t_certificate', 'certificate', standard_opts, null); };
   factory.CustomAttribute        = ()=>{ return create_dao('t_custom_attribute', 'custom_attribute', standard_opts, null); };
-  factory.ProductCustomAttribute = ()=>{ return create_dao('t_product_custom_attribute', 'product_custom_attribute', standard_opts, null); };
   factory.Equipment              = ()=>{ return create_dao('t_equipment', 'equipment', standard_opts, null); };
   factory.EquipmentGroup         = ()=>{ return create_dao('t_equipment_group', 'equipment_group', standard_opts, null); };
   factory.EquipmentImage         = ()=>{ return create_dao('t_equipment_image', 'equipment_image', standard_opts, null); };
   factory.EquipmentType          = ()=>{ return create_dao('t_equipment_type', 'equipment_type', standard_opts, null); };
   factory.Family                 = ()=>{ return create_dao('t_family', 'family', standard_opts, null); };
-  factory.FamilyGroup            = ()=>{ return create_dao('t_family_group', 'family_group', standard_opts, null); };
   factory.Filter                 = ()=>{ return create_dao('t_filter', 'filter', standard_opts, null); };
   factory.FilterOption           = ()=>{ return create_dao('t_filter_option', 'filter_option', standard_opts, null); };
+  factory.Formula                = ()=>{ return create_dao('t_formula', 'formula', standard_opts, null); };
   factory.Group                  = ()=>{ return create_dao('t_group', 'group', standard_opts, null); };
   factory.ImageType              = ()=>{ return create_dao('t_image_type', 'image_type', standard_opts, null); };
   factory.Lifecycle              = ()=>{ return create_dao('t_lifecycle', 'lifecycle', standard_opts, null); };
   factory.PackagingFactor        = ()=>{ return create_dao('t_packaging_factor', 'packaging_factor', standard_opts, null); };
   factory.Product                = ()=>{ return create_dao('t_product', 'product', standard_opts, null); };
   factory.ProductCertificate     = ()=>{ return create_dao('t_product_certificate', 'product_certificate', standard_opts, null); };
+  factory.ProductCustomAttribute = ()=>{ return create_dao('t_product_custom_attribute', 'product_custom_attribute', standard_opts, null); };
   factory.ProductEquipment       = ()=>{ return create_dao('t_product_equipment_connect', 'product_equipment_connect', standard_opts, null); };
   factory.ProductFamily          = ()=>{ return create_dao('t_product_family_connect', 'product_family_connect', standard_opts, null); };
+  factory.ProductFilterOption    = ()=>{ return create_dao('t_product_filter_option', 'product_filter_option', standard_opts, null); };
   factory.ProductImage           = ()=>{ return create_dao('t_product_image', 'product_image', standard_opts, null); };
   factory.ProductOemReference    = ()=>{ return create_dao('t_product_oem_reference', 'product_oem_reference', standard_opts, null); };
-  factory.ProductFilterOption    = ()=>{ return create_dao('t_product_filter_option', 'product_filter_option', standard_opts, null); };
   factory.ProductSet             = ()=>{ return create_dao('t_product_set', 'product_set', standard_opts, null); };
   factory.ProductType            = ()=>{ return create_dao('t_product_type', 'product_type', standard_opts, null); };
   factory.Supplier               = ()=>{ return create_dao('t_supplier', 'supplier', standard_opts, null); };
-
-  factory.Pet                    = ()=>{ return create_dao('t_test_pet', 'pet', standard_opts, null); }; //for testing only.
 
   //views
   factory.CategoryView                  = ()=>{ return create_dao('v_category', 'category_view', null, null); };
@@ -101,6 +99,8 @@ module.exports = function (pool) {
         return factory.Filter();
       case 'filter_option':
         return factory.FilterOption();
+      case 'formula':
+        return factory.Formula();
       case 'group':
         return factory.Group();
       case 'image_type':
@@ -130,9 +130,6 @@ module.exports = function (pool) {
       case 'supplier':
         return factory.Supplier();
 
-      case 'pet':
-        return factory.Pet();
-      //TODO: views?
       default:
         return null;
     }
